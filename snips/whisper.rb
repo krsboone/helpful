f9318a7c7c89cdba919d2a895e806851e56bcf87
krsboone <git@krisboone.com>
2013-03-10 22:48:25 -0300

    Update whisper.rb

--- snips/whisper.rb
+++ snips/whisper.rb
@@ -2,7 +2,7 @@ say "One moment while we connect your call"
 transfer '14075555555', {
     :playvalue => "http://hosting.tropo.com/someAccountID/www/audio/ringAudioFile.wav",
     :playTones => true,
-    :callerID => "14075555555",
+    :callerID => "14075555556",
     :onConnect => lambda { |event| 
         ask "Press 1 to accept the call, press any other key to reject.", {
             :choices => "1",
