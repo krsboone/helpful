e14399150b3313467f6a1933caa039778df11fe0
krsboone <git@krisboone.com>
2013-03-10 20:35:24 -0400

    initial file
    first go

---
+++ snips/whisper.rb
@@ -0,0 +1,20 @@
+say "One moment while we connect your call"
+transfer '14075555555', {
+    :playvalue => "http://hosting.tropo.com/46256/www/audio/bookofthemonkey.mp3",
+    :playTones => true,
+    :callerID => "14075555555",
+    :onConnect => lambda { |event| 
+        ask "Press 1 to accept the call, press any other key to reject.", {
+            :choices => "1",
+            :timeout => 15.0,
+            :mode => "dtmf",
+            :onChoice => lambda { |event|
+                 say "Connecting you now"
+            },
+            :onBadChoice => lambda { |event|
+                 say "Rejecting the call, goodbye"
+                 hangup
+            }
+         }
+     }
+}
